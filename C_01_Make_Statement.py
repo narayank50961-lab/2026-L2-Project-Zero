9f6d2d3870dda4008c0a2dceac0c6e92457c6fd8
narayank50961-lab <narayank50961@masseyhigh.school.nz>
2026-02-11 10:52:10 +1300

    Functions to create single line decorated headings

---
+++ C_01_Make_Statement.py
@@ -0,0 +1,9 @@
+# Functions go here
+def make_statment( statement, decoration):
+     """Emphasises heading by adding decoration
+     at the start and end"""
+
+     print(f"{decoration  * 3} {statement} {decoration * 3}")
+
+# Main Routine goes here=
+make_statment("Programming is Fun!", "👍")
