49e5026de157f7f95334c57aed0fefdada5002b3
narayank50961-lab <narayank50961@masseyhigh.school.nz>
2026-02-26 08:31:07 +1200

    V2

---
+++ 2026-MMF/C_05_name_age_pay.py
@@ -0,0 +1,58 @@
+# Functions go here
+def int_check(question):...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def not_blank(question):...
+
+
+
+
+
+
+
+
+
+
+
+
+
+def string_check(question, valid_answers=('yes','no'), num_letters=1):...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Main Routine goes here
+
+# internalise variables / non-default options for string checker
+payment_ans= ('cash','credit')
+
+# loop for testing purposes...
+while True:
+    print()
